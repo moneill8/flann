a54d8b7a1a2fc8b87eb40a58791d27e06cfd8878
oneilm6 <oneilm6@ubuntu.ubuntu-domain>
2015-05-05 23:19:16 -0500

    actually added it finally

--- src/cpp/flann/algorithms/all_indices.h
+++ src/cpp/flann/algorithms/all_indices.h
@@ -41,6 +41,7 @@
 #include "flann/algorithms/hierarchical_clustering_index.h"
 #include "flann/algorithms/lsh_index.h"
 #include "flann/algorithms/autotuned_index.h"
+#include "flann/algorithms/graph_index.h"
 #ifdef FLANN_USE_CUDA
 #include "flann/algorithms/kdtree_cuda_3d_index.h"
 #endif
@@ -181,6 +182,9 @@ inline NNIndex<Distance>*
 		break;
 	case FLANN_INDEX_LSH:
 		nnIndex = create_index_<LshIndex,Distance,ElementType>(dataset, params, distance);
+		break;
+	case GRAPH_INDEX:
+		nnIndex = create_index_<GraphIndex,Distance,ElementType>(dataset, params, distance);
 		break;
 	default:
 		throw FLANNException("Unknown index type");
--- src/cpp/flann/defines.h
+++ src/cpp/flann/defines.h
@@ -88,7 +88,8 @@ enum flann_algorithm_t
 #ifdef FLANN_USE_CUDA
     FLANN_INDEX_KDTREE_CUDA 	= 7,
 #endif
-    FLANN_INDEX_SAVED 			= 254,
+    GRAPH_INDEX                 = 8,
+	FLANN_INDEX_SAVED 			= 254,
     FLANN_INDEX_AUTOTUNED 		= 255,
 };
 
